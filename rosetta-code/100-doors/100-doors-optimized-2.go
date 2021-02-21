c033c5783884d76e512933c076b933f3a15809b3
Raghav Dinesh <ruwgxo@gmail.com>
2021-02-21 14:24:37 +0000

    Initial Commit: Rosetta protocol init

---
+++ rosetta-code/100-doors/100-doors-optimized-2.go
@@ -0,0 +1,19 @@
+// 100 (optimized - 2 ) doors in Go
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		f := math.Sqrt(float64(i))
+		if math.Mod(f, 1) == 0 {
+			fmt.Print("O")
+		} else {
+			fmt.Print("-")
+		}
+	}
+	fmt.Println()
+}
